dc61f894ed6bf2ecf0e392dbb410ef3f05c6051c
salman_at_github <salman1m22@gmail.com>
2020-05-07 07:56:25 +0600

    static item listing

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Name_R1', 'Description_D1', 'https://www.wholesomeyum.com/wp-content/uploads/2019/09/wholesomeyum-keto-chaffles-recipe-sweet-savory-5-ways-24.jpg'),
+    new Recipe('Name_R2', 'Description_D2', 'https://www.wholesomeyum.com/wp-content/uploads/2019/09/wholesomeyum-the-best-broiled-lobster-tail-recipe-10.jpg'),
+    new Recipe('Name_R2', 'Description_D2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQHQEBsSWwX5mh3njzPZ7jmZcCqTcKKczg5e1ZParyAdmsRM1uO&usqp=CAU')
+  ];
 
   constructor() { }
 
